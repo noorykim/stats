17cad174f31e0340821bca750bcdd0fc42c5c549
Noory Kim <noorykim@users.noreply.github.com>
2018-02-07 16:07:35 -0500

    Update kruskal_wallis_adjust_for_ties.r

--- kruskal_wallis_adjust_for_ties.r
+++ kruskal_wallis_adjust_for_ties.r
@@ -8,6 +8,7 @@ kw = 12/(27*28)*(159^2/9+149.5^2/9+69.5^2/9)-3*28
 # There were 7 twos, 12 threes, and 7 fours
 adj_factor = 1 - sum(7^3-7, 12^3-12, 7^3-7)/(27^3-27)
 
+# Kruskal-Wallis test statistic, after adjusting for ties
 kw /adj_factor
 
 
