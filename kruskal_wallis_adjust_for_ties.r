01a6a0ecbdc7afaa8123242950026c6677211351
Noory Kim <noorykim@users.noreply.github.com>
2018-02-07 16:05:48 -0500

    Create kruskal_wallis_adjust_for_ties.r

---
+++ kruskal_wallis_adjust_for_ties.r
@@ -0,0 +1,13 @@
+# Kruskal-Wallis test statistic, without adjusting for ties
+# There were 27 scores altogether, 9 each from 3 groups
+kw = 12/(27*28)*(159^2/9+149.5^2/9+69.5^2/9)-3*28
+
+# Adjust for ties
+# There were 7 twos, 12 threes, and 7 fours
+adj_factor = 1 - sum(7^3-7, 12^3-12, 7^3-7)/(27^3-27)
+
+kw /adj_factor
+
+
+# References:
+# http://support.minitab.com/en-us/minitab-express/1/help-and-how-to/modeling-statistics/anova/how-to/kruskal-wallis-test/methods-and-formulas/methods-and-formulas/
